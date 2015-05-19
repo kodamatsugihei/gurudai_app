e6027491bbf63b8d45413466d7979aa31adbf414
児玉継兵 <javajava552000@yahoo.co.jp>
2015-05-19 06:37:14 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+GurudaiApp2::Application.config.secret_key_base = '6edb9e1389e225554caaafd590b698a012c800a0eaf37115d400e5b332fe6a2a2ba01ef243d7b7a6e207f8175f21a0238177e89b0ddca602e494ce83e880fad0'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+GurudaiApp2::Application.config.session_store :cookie_store, key: '_gurudai_app2_session'
